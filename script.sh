0622bfc3129c3628030904bbd2bc9e7b8c2c319f
SadhanaAWS <43659646+SadhanaAWS@users.noreply.github.com>
2019-06-04 14:31:40 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,11 @@
 echo "SADHANA"
 
 #cd /root/atlassian-bamboo-6.9.0/bin/C:/bamboo/bamboo-home/xml-data/build-dir/FLUXWP-FLUXWPPLAN-JOB1
-
-kubectl delete -f w-deploy.yaml
-sleep 10
-kubectl create -f w-deploy.yaml
-
+#kubectl delete -f w-deploy.yaml
+#sleep 10
+#kubectl create -f w-deploy.yaml
 
 echo ${bamboo.buildNumber}
-
+print ${bamboo.buildNumber}
 export TAG={bamboo.buildNumber}
 echo $TAG
